aa626ef2410d1dc5517e31c20a44a803869b3219
vat <tuanvahe2021@gmail.com>
2023-05-17 17:37:58 +0700

    Add demo

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonService } from '../Services/common.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+  public loginName = "admin";
+  public myColor ="red";
+  public counter = 0;
+  public counterBinhPhuong = 0;
+
+  constructor(private common: CommonService){
+
+  }
+
+  ngOnInit():void{
+    this.counter = this.common.counter;
+    this.counterBinhPhuong = this.common.binhPhuong(this.counter);
+  }
+
+}
